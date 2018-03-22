85e9f1e33f678fdfeb3bddf2301eec6fbef36e39
LAPTOP-3828POI3\Robert <robert.kirchhecker@gmail.com>
2018-03-22 18:34:36 +0100

    ADD:Ki_KöniginUndLarvenEntwcklung
    Die Königin spawnt Eier wenn sie gebumst wird.
    Diese Eierer können sich nach einer beliebig langer zeit entwickeln und zu einer indivituellen ameisen werden.

---
+++ Ant-Simulator/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private GameObject ant;
+
+    private GameObject antPool;
+    public static int MaxAnts = 30;
+    public static int CurrentAnts;
+
+    public static Queue<GameObject> Ants = new Queue<GameObject>();
+
+    private void Start()
+    {
+        antPool = GameObject.Find("AntPool");
+        ant = GameObject.Find("operarius cibo formica");
+        for (int i = 0; i < MaxAnts; i++)
+        {
+            GameObject temp = Instantiate(ant, antPool.transform);
+            Ants.Enqueue(temp);
+            temp.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Ant-Simulator/Assets/Scripts/Rigina_formica editor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(KI_Rigina_formica))]
+public class Rigina_formicaeditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        KI_Rigina_formica myscript = (KI_Rigina_formica)target;
+        if (GUILayout.Button("Bumsen"))
+        {
+            myscript.SpawnLarva();
+        }
+    }
+}

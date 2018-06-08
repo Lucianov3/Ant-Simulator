0ab0a5182c1afc2711382233aec7221215d44874
Thor <erikdamisch@live.com>
2018-06-08 15:15:53 +0200

    Blocks, NavMeshSurface Fixed Movement

--- Ant-Simulator/Assets/Scripts/SpawnBlockBuildNavMesh.cs
+++ Ant-Simulator/Assets/Scripts/SpawnBlockBuildNavMesh.cs
@@ -9,11 +9,13 @@ public class SpawnBlockBuildNavMesh : MonoBehaviour
     private NavMeshSurface surface;
     private int temp;
     private bool isBuild;
+    private NavMeshSurface[] surfaces;
 
 	void Start ()
     {
         isBuild = true;
-        temp = 0;
+        temp = 10;
+        surfaces = new NavMeshSurface[10];
     }
 
 	void Update ()
@@ -22,10 +24,15 @@ public class SpawnBlockBuildNavMesh : MonoBehaviour
         {
             for (int i = 0; i < 10; i++)
             {
-                GameObject DirtBlockClone = (GameObject)Instantiate(DirtBlock, new Vector3(0, 0, temp), Quaternion.Euler(0, 0, -90));
-                surface = DirtBlockClone.GetComponentInChildren<NavMeshSurface>();
-                surface.BuildNavMesh();
-                temp += 10;
+                    GameObject DirtBlockClone = (GameObject)Instantiate(DirtBlock, new Vector3(0, 0, temp), Quaternion.Euler(0, 0, -90));
+                    surface = DirtBlockClone.GetComponentInChildren<NavMeshSurface>();
+                    surfaces[i] = DirtBlockClone.GetComponentInChildren<NavMeshSurface>();
+                    surface.BuildNavMesh();
+                    temp += 10;
+            }
+            for (int j = 0; j < surfaces.Length; j++)
+            {
+                surfaces[j].BuildNavMesh();
             }
             isBuild = false;
         }

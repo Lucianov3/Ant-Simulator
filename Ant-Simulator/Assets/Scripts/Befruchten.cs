eae6bcffa322801f26b923e7cbff3006ba8a4e17
LAPTOP-3828POI3\Robert <robert.kirchhecker@gmail.com>
2018-06-18 19:09:53 +0200

    ADD: StateMachine Arbeiter

---
+++ Ant-Simulator/Assets/Scripts/Befruchten.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Befruchten : StateMachineBehaviour
+{
+    private NavMeshAgent ant;
+    public Vector3 Targetposition;
+    private KI_Rigina_formica queen;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        queen = new KI_Rigina_formica();
+        ant = animator.GetComponent<NavMeshAgent>();
+        ant.SetDestination(Targetposition);
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Vector3.Distance(Targetposition, ant.transform.position) < 1)
+        {
+            queen.SpawnLarva();
+        }
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+    }
+}
